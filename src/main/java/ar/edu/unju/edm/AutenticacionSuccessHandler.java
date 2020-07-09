37761a62f2d5cbe1fbeabb1ae6547e30473ae33c
camic <camic@192.168.0.7>
2020-07-09 18:57:35 -0300

    Eliminación Html login Agregado WebSecurityConfig y AutenticacionSuccessHandler

---
+++ src/main/java/ar/edu/unju/edm/AutenticacionSuccessHandler.java
@@ -0,0 +1,59 @@
+package ar.edu.unju.edm;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AutenticacionSuccessHandler implements AuthenticationSuccessHandler {
+
+	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+	@Override
+	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+			Authentication authentication) throws IOException, ServletException {
+		boolean tipoRegistrador = false;
+		boolean tipoConsultor = false;
+		boolean tipoBD = false;
+		Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+		for (GrantedAuthority grantedAuthority : authorities) {
+			if(grantedAuthority.getAuthority().equals("Registrador")) {
+				tipoRegistrador = true;
+				break;
+			}else {
+				if(grantedAuthority.getAuthority().equals("Consultor")) {
+					tipoConsultor = true;
+					break;
+				}else {
+					tipoBD = true;
+					break;
+			}
+				
+			}
+		}
+		if(tipoRegistrador) {
+			redirectStrategy.sendRedirect(request, response, "/registrador");
+		}else {
+			if(tipoConsultor) {
+				redirectStrategy.sendRedirect(request, response, "/consultor");
+			}else {
+				if(tipoBD) {
+					redirectStrategy.sendRedirect(request, response, "/usuarioBD");
+				}else {
+					throw new IllegalStateException();
+				}
+			}
+		}
+	}
+}
